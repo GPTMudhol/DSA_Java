3bd6ccb166ae769f725ba978dfd6a66613be227e
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-23 11:16:24 +0530

    Fix Movezeros method and main method syntax
    Fix method signature and return statement in Movezeros method. Correct main method syntax and print statement.

--- Arrays/Two_Pointers/09_Move_Zeros_To_End.java
+++ Arrays/Two_Pointers/09_Move_Zeros_To_End.java
@@ -1,1 +1,32 @@
+class Solution{
+  public int [] Movezeros(int [] nums){
+    int nz = 0;
+    int z = 0;
 
+    if (nums.length == 0 || nums.length ==1){
+      return;
+    }
+
+    while(nz < nums.length){
+      if(nums[nz] != 0){
+        int temp = nums[nz];
+        nums[nz] = nums[z];
+        nums[z] = temp;
+        nz++;
+        z++;
+      }else{
+        nz++;
+      }
+    }
+
+  }
+}
+
+public class Main{
+  public static void main (String [] args]{
+       int [] nums = {-4,-3,-2,0,2,4,5};
+    Solution obj = new Solution ();
+    Sop(obj.Movezeros(nums));
+  }
+}
+    
