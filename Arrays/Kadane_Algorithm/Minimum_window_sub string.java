910f27f61023fc82e15a1e16a17afe0926687efc
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-25 17:22:38 +0530

    Implement minimum window substring algorithm

--- Arrays/Kadane_Algorithm/Minimum_window_sub string.java
+++ Arrays/Kadane_Algorithm/Minimum_window_sub string.java
@@ -1,1 +1,52 @@
 
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+
+        Map<Character, Integer> tMap = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+
+        Map<Character, Integer> windowMap = new HashMap<>();
+
+        int left = 0, right = 0;
+        int formed = 0;
+        int required = tMap.size();
+
+        int minLen = Integer.MAX_VALUE;
+        int start = 0;
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            windowMap.put(c, windowMap.getOrDefault(c, 0) + 1);
+
+            if (tMap.containsKey(c) && 
+                windowMap.get(c).intValue() == tMap.get(c).intValue()) {
+                formed++;
+            }
+
+            while (left <= right && formed == required) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char leftChar = s.charAt(left);
+                windowMap.put(leftChar, windowMap.get(leftChar) - 1);
+
+                if (tMap.containsKey(leftChar) && 
+                    windowMap.get(leftChar) < tMap.get(leftChar)) {
+                    formed--;
+                }
+
+                left++;
+            }
+
+            right++;
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" 
+               : s.substring(start, start + minLen);
+    }
+}
