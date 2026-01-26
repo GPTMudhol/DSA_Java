bb7b92921c79ad6ec97d534c96faeeedbfdb2063
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-26 08:25:03 +0530

    Implement Kadane's algorithm for subarray product

--- Arrays/Kadane_Algorithm/05_Subarray_product_less_than_K.java
+++ Arrays/Kadane_Algorithm/05_Subarray_product_less_than_K.java
@@ -1,1 +1,19 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int product =1, start = 0,end =0;
+        int count = 0;
+        int n = nums.length;
 
+        while(end < n){
+            product *= nums[end];
+
+            while(product >= k && start <= end){
+                product /= nums[start];
+                start++;
+            }
+            count += 1 + (end - start);
+            end++;
+        }
+        return count;
+    }
+}
