d99cd92d4845317dbad343a129b4ea7fbda3e957
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-28 07:48:32 +0530

    Add method to find all anagrams in a string
    Implemented a solution to find all starting indices of anagrams of string p in string s using a sliding window approach.

--- Arrays/Kadane_Algorithm/07_Find_All_Anagrams_in_a_String_.java
+++ Arrays/Kadane_Algorithm/07_Find_All_Anagrams_in_a_String_.java
@@ -1,1 +1,65 @@
 
+import java.util.*;
+
+class Solution {
+
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+
+        if (s.length() < p.length()) {
+            return result;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        // Build frequency map for p
+        for (char c : p.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int left = 0, right = 0;
+        int matched = 0;
+
+        while (right < s.length()) {
+            char rightChar = s.charAt(right);
+
+            if (map.containsKey(rightChar)) {
+                map.put(rightChar, map.get(rightChar) - 1);
+                if (map.get(rightChar) == 0) {
+                    matched++;
+                }
+            }
+            right++;
+
+            // When window size equals p length
+            if (right - left == p.length()) {
+                if (matched == map.size()) {
+                    result.add(left);
+                }
+
+                char leftChar = s.charAt(left);
+                if (map.containsKey(leftChar)) {
+                    if (map.get(leftChar) == 0) {
+                        matched--;
+                    }
+                    map.put(leftChar, map.get(leftChar) + 1);
+                }
+                left++;
+            }
+        }
+
+        return result;
+    }
+
+ 
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String s = "cbaebabacd";
+        String p = "abc";
+
+        List<Integer> output = solution.findAnagrams(s, p);
+
+        System.out.println("Anagram starting indices: " + output);
+    }
+}
