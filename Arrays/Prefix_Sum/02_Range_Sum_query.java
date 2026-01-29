c3efaa20bfd223c97776edb983ba144eb6d6043c
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-29 18:08:02 +0530

    Implement NumArray with prefix sum functionality

--- Arrays/Prefix_Sum/02_Range_Sum_query.java
+++ Arrays/Prefix_Sum/02_Range_Sum_query.java
@@ -1,1 +1,33 @@
 
+class NumArray {
+
+    int[] prefix;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        prefix = new int[n];
+
+        prefix[0] = nums[0];
+
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0)
+            return prefix[right];
+        return prefix[right] - prefix[left - 1];
+    }
+
+    
+    public static void main(String[] args) {
+        int[] nums = { -2, 0, 3, -5, 2, -1 };
+
+        NumArray obj = new NumArray(nums);
+
+        System.out.println(obj.sumRange(0, 2)); // Output: 1
+        System.out.println(obj.sumRange(2, 5)); // Output: -1
+        System.out.println(obj.sumRange(0, 5)); // Output: -3
+    }
+}
