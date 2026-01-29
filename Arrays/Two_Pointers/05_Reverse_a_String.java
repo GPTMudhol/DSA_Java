9cdadb47fa050f30cfe6e4d27ee4f2198c572293
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-29 18:13:51 +0530

    Add reverseString method to reverse a character array
    Implement a method to reverse a character array in place.

--- Arrays/Two_Pointers/05_Reverse_a_String.java
+++ Arrays/Two_Pointers/05_Reverse_a_String.java
@@ -1,1 +1,28 @@
 
+import java.util.Arrays;
+
+public class Solution {
+
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while (start < end) {
+            char temp = s[start];
+            s[start] = s[end];
+            s[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+        sol.reverseString(s);
+
+        System.out.println(Arrays.toString(s));
+        // Output: [o, l, l, e, h]
+    }
+}
