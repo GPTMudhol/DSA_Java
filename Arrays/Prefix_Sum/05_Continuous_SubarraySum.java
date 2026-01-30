e14f6b3240434c55c3a4c8c7ef3b8854517614fe
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-30 08:42:24 +0530

    Implement checkSubarraySum method for subarray check

--- Arrays/Prefix_Sum/05_Continuous_SubarraySum.java
+++ Arrays/Prefix_Sum/05_Continuous_SubarraySum.java
@@ -1,1 +1,23 @@
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>(){
+            {
+                put(0,-1);
+            }
+        };
 
+        int prefixsummodk = 0;
+
+        for(int i = 0; i < nums.length; i++){
+            prefixsummodk += nums[i];
+            prefixsummodk %= k;
+
+            if(!map.containsKey(prefixsummodk)){
+                map.put(prefixsummodk,i);
+            }else if(i - map.get(prefixsummodk) > 1){
+                return true;
+            }
+        }
+        return false; 
+    }
+}
