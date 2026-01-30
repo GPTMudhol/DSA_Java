b3079dc0e1438e2e58662184fc1e549b0d06b72c
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-30 21:41:39 +0530

    Update 06_Product_of_array_expect_self.java

--- Arrays/Prefix_Sum/06_Product_of_array_expect_self.java
+++ Arrays/Prefix_Sum/06_Product_of_array_expect_self.java
@@ -1,1 +1,32 @@
-06
+ Brute Force Approach (O(n²))
+ 
+public class ProductExceptSelfBrute {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int product = 1;
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    product *= nums[j];
+                }
+            }
+            res[i] = product;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] ans = productExceptSelf(nums);
+
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+    }
+}
+
+
+
