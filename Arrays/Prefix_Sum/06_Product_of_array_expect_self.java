cf6337d4096b9d37e424e4be9c1fe7a9b4aa8034
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-30 21:42:09 +0530

    Add optimal approach for product of array except self

--- Arrays/Prefix_Sum/06_Product_of_array_expect_self.java
+++ Arrays/Prefix_Sum/06_Product_of_array_expect_self.java
@@ -1,4 +1,6 @@
- Brute Force Approach (O(n²))
+
+
+Brute Force Approach (O(n²))
  
 public class ProductExceptSelfBrute {
 
@@ -30,3 +32,44 @@ public class ProductExceptSelfBrute {
 
 
 
+Optimal Approach (O(n) time, no division)
+  
+  public class ProductExceptSelfOptimal {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] res = new int[n];
+
+        left[0] = 1;
+        for (int i = 1; i < n; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+
+        right[n - 1] = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < n; i++) {
+            res[i] = left[i] * right[i];
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        int[] ans = productExceptSelf(nums);
+
+        for (int x : ans) {
+            System.out.print(x + " ");
+        }
+    }
+}
+
+
+
+06
