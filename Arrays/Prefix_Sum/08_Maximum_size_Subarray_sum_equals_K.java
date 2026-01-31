54cf499a08c7545665032a90dfce8b7b9421898d
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-31 09:03:30 +0530

    Implement optimal solution for max subarray sum equals K

--- Arrays/Prefix_Sum/08_Maximum_size_Subarray_sum_equals_K.java
+++ Arrays/Prefix_Sum/08_Maximum_size_Subarray_sum_equals_K.java
@@ -28,5 +28,36 @@ public class BruteForceMaxSizeSubarray {
 
 
 // Optimal Approach 
+import java.util.HashMap;
 
+public class OptimalMaxSizeSubarray {
+
+    public static int maxSubArrayLen(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+
+        int sum = 0;
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            if (map.containsKey(sum - k)) {
+                maxLen = Math.max(maxLen, i - map.get(sum - k));
+            }
+
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, -1, 5, -2, 3};
+        int k = 3;
+
+        System.out.println(maxSubArrayLen(nums, k)); // Output: 4
+    }
+}
 
