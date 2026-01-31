46899d20463a7383e3d4d5e3629e6ce09c683341
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-31 08:52:24 +0530

    Add optimal solution for running sum of 1D array

--- Arrays/Prefix_Sum/07_Running_sum_of_1D_array.java
+++ Arrays/Prefix_Sum/07_Running_sum_of_1D_array.java
@@ -17,3 +17,25 @@ class Solution {
 }
 
 
+// optimal Solution 
+
+class Solution {
+
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = nums[i] + nums[i - 1];
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        int[] nums = {1, 2, 3, 4};
+        int[] result = obj.runningSum(nums);
+
+        for (int x : result) {
+            System.out.print(x + " ");
+        }
+    }
+}
