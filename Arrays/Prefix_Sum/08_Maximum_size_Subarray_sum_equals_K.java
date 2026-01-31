64506bacf09befcf5c78c7d6bd1f9dd611b790fd
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-01-31 09:02:01 +0530

    Add brute force solution for max size subarray sum
    Implemented brute force approach to find maximum size subarray with sum equals to k.

--- Arrays/Prefix_Sum/08_Maximum_size_Subarray_sum_equals_K.java
+++ Arrays/Prefix_Sum/08_Maximum_size_Subarray_sum_equals_K.java
@@ -1,1 +1,32 @@
+// Brute Force Approach 
+
+public class BruteForceMaxSizeSubarray {
+
+    public static int maxSubArrayLen(int[] nums, int k) {
+        int maxLen = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+
+                if (sum == k) {
+                    maxLen = Math.max(maxLen, j - i + 1);
+                }
+            }
+        }
+        return maxLen;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, -1, 5, -2, 3};
+        int k = 3;
+
+        System.out.println(maxSubArrayLen(nums, k)); // Output: 4
+    }
+}
+
+
+// Optimal Approach 
+
 
