938a3d6cc546b793fbf28b0f34cf77a3d17826aa
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-01 08:13:57 +0530

    Add brute force solution for car pooling problem
    Implemented a brute force approach to check car pooling capacity using an array to track passenger counts on the road.

--- Arrays/Prefix_Sum/11_Car_Pooling.java
+++ Arrays/Prefix_Sum/11_Car_Pooling.java
@@ -1,1 +1,24 @@
+// Brute Force Approach O(n*D) 
+(N = trips, D = distance between start & end)
+  
 
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] road = new int[1001];
+
+        for (int[] trip : trips) {
+            int passengers = trip[0];
+            int start = trip[1];
+            int end = trip[2];
+
+            for (int i = start; i < end; i++) {
+                road[i] += passengers;
+                if (road[i] > capacity) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
