7d00119643f67989cd7686cbcd5dc1191543835c
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-01 08:10:15 +0530

    Add brute force method for counting nice subarrays
    Implement brute force approach to count nice subarrays.

--- Arrays/Prefix_Sum/10_Count_Number_of_Nice_subarrays.java
+++ Arrays/Prefix_Sum/10_Count_Number_of_Nice_subarrays.java
@@ -1,1 +1,26 @@
+// Brute Force Approach O(n^2)
 
+
+public class Solution {
+
+    public static int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int odd = 0;
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 != 0) odd++;
+                if (odd == k) count++;
+                if (odd > k) break;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 1, 1};
+        int k = 3;
+        System.out.println(numberOfSubarrays(nums, k));
+    }
+}
