d5b41023f903e82961f9784e86bbe6bcd26a9f3e
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-01 08:11:05 +0530

    Add method to count nice subarrays using prefix sum
    Implement optimal approach to count nice subarrays.

--- Arrays/Prefix_Sum/10_Count_Number_of_Nice_subarrays.java
+++ Arrays/Prefix_Sum/10_Count_Number_of_Nice_subarrays.java
@@ -24,3 +24,34 @@ public class Solution {
         System.out.println(numberOfSubarrays(nums, k));
     }
 }
+
+
+// Optimal Approach O(n)
+
+public class Solution {
+
+    public static int numberOfSubarrays(int[] nums, int k) {
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
+
+    private static int atMost(int[] nums, int k) {
+        int left = 0, count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] % 2 != 0) k--;
+
+            while (k < 0) {
+                if (nums[left] % 2 != 0) k++;
+                left++;
+            }
+            count += right - left + 1;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 2, 1, 1};
+        int k = 3;
+        System.out.println(numberOfSubarrays(nums, k));
+    }
+}
