24bbba60b4fdb9097c7e7c7fe82bd4322b8b6f4d
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-02 19:12:38 +0530

    Implement brute force solution for Two Sum problem

--- 01_Two_Sum_problem.java
+++ 01_Two_Sum_problem.java
@@ -1,1 +1,20 @@
+// Brute Force Approach 
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] res = twoSum(new int[]{2,7,11,15}, 9);
+        System.out.println(res[0] + " " + res[1]);
+    }
+}
