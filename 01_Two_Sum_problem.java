87d2e44b780706eb6f2539e294e83d8095232a80
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-02 19:13:11 +0530

    Add optimal two-sum function using HashMap
    Implemented optimal two-sum solution using HashMap.

--- 01_Two_Sum_problem.java
+++ 01_Two_Sum_problem.java
@@ -18,3 +18,25 @@ class Solution {
         System.out.println(res[0] + " " + res[1]);
     }
 }
+
+
+// Optimal Approach 
+
+import java.util.HashMap;
+
+class Solution {
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[]{map.get(need), i};
+            }
+
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
