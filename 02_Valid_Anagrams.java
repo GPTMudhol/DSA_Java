3d34fed8ce334d918cd5dd1a637526be3bbe88f1
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-02 19:09:25 +0530

    Implement isAnagram method using sorting

--- 02_Valid_Anagrams.java
+++ 02_Valid_Anagrams.java
@@ -1,1 +1,23 @@
+// Brute Force Approach 
 
+
+import java.util.Arrays;
+
+class Solution {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        return Arrays.equals(a, b);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "nagaram")); // true
+        System.out.println(isAnagram("rat", "car"));         // false
+    }
+}
