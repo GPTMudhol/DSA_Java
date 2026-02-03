3cbccc374f275db8fac0b302d7e43de862116b53
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-03 20:00:26 +0530

    Update 04_Top_K_Frequent_Elements.java

--- 04_Top_K_Frequent_Elements.java
+++ 04_Top_K_Frequent_Elements.java
@@ -1,1 +1,27 @@
+// Brute Force Approach 
+// Time Complexity O(n log n)
+// Space Complexity O(n)
 
+class Solution {
+
+    public int[] topKFrequent(int[] nums, int k) {
+
+        HashMap<Integer, Integer> freqMap = new HashMap<>();
+
+        for (int num : nums) {
+            freqMap.put(num, freqMap.getOrDefault(num, 0) + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> list =
+                new ArrayList<>(freqMap.entrySet());
+
+        list.sort((a, b) -> b.getValue() - a.getValue());
+
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = list.get(i).getKey();
+        }
+
+        return result;
+    }
+}
