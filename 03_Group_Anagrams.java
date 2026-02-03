298290daa1fdf852296187567a53f51a2cace35e
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-03 19:52:04 +0530

    Add brute force solution for grouping anagrams
    Implemented a brute force approach to group anagrams with O(n² * m log m) time complexity.

--- 03_Group_Anagrams.java
+++ 03_Group_Anagrams.java
@@ -1,1 +1,45 @@
+/// Brute Force Approach 
+// Time Complexity: O(n² * m log m)
+// Space Complexity: O(n)
 
+class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        boolean[] used = new boolean[strs.length];
+        List<List<String>> result = new ArrayList<>();
+
+        for (int i = 0; i < strs.length; i++) {
+
+            if (used[i]) continue;
+
+            List<String> group = new ArrayList<>();
+            group.add(strs[i]);
+            used[i] = true;
+
+            for (int j = i + 1; j < strs.length; j++) {
+                if (!used[j] && isAnagram(strs[i], strs[j])) {
+                    group.add(strs[j]);
+                    used[j] = true;
+                }
+            }
+
+            result.add(group);
+        }
+
+        return result;
+    }
+
+    private boolean isAnagram(String a, String b) {
+
+        if (a.length() != b.length()) return false;
+
+        char[] ca = a.toCharArray();
+        char[] cb = b.toCharArray();
+
+        Arrays.sort(ca);
+        Arrays.sort(cb);
+
+        return Arrays.equals(ca, cb);
+    }
+}
