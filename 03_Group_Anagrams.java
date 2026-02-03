1e84a1676e850a70da2eb360134f73a3d130210e
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-03 19:53:55 +0530

    Add optimal anagram grouping solution
    Implemented an optimal approach to group anagrams using a hashmap.

--- 03_Group_Anagrams.java
+++ 03_Group_Anagrams.java
@@ -43,3 +43,35 @@ class Solution {
         return Arrays.equals(ca, cb);
     }
 }
+
+
+// Optimal Approach 
+// Time Complexity: O(n * m)
+// Space Complexity: O(n)
+
+class Solution {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+
+            int[] count = new int[26];
+
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder key = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                key.append('#').append(count[i]);
+            }
+
+            map.putIfAbsent(key.toString(), new ArrayList<>());
+            map.get(key.toString()).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
