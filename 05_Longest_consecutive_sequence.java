6ff20dbec57bf95235914cbca4b1d0ce85ff03df
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-04 08:23:07 +0530

    Clarify time and space complexity in comments
    Updated comments for clarity on time and space complexity.

--- 05_Longest_consecutive_sequence.java
+++ 05_Longest_consecutive_sequence.java
@@ -1,1 +1,33 @@
+// Brute Force Approach 
+// Time Complexity O(n^2)
+// Space Complexity O (1)
+
+
+public class Solution {
+
+    static boolean linearSearch(int[] arr, int num) {
+        for (int x : arr) {
+            if (x == num) return true;
+        }
+        return false;
+    }
+
+    public static int longestConsecutive(int[] arr) {
+        int n = arr.length;
+        int longest = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = arr[i];
+            int count = 1;
+
+            while (linearSearch(arr, x + 1)) {
+                x++;
+                count++;
+            }
+            longest = Math.max(longest, count);
+        }
+        return longest;
+    }
+}
+
 
