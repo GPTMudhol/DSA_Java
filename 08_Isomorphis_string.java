d370cb9499e8044d4ec7d1af0c5d151e47c1490a
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-05 20:34:35 +0530

    Implement isIsomorphic method in Solution class
    Added a method to check if two strings are isomorphic.

--- 08_Isomorphis_string.java
+++ 08_Isomorphis_string.java
@@ -1,1 +1,15 @@
 
+// Time Complexity: O(n²)
+// Space Complexity: O(1)
+
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
