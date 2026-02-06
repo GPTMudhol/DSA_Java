431f3ec8fb874b9e5da508b03e2dae28b3236ac3
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-06 10:05:19 +0530

    Update 07_Ransom_Notes_Problem.java

--- 07_Ransom_Notes_Problem.java
+++ 07_Ransom_Notes_Problem.java
@@ -1,1 +1,18 @@
+// Brute Force Approach 
+// Time Complexity: O(n * m)
+// Space Complexity: O(m)
 
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        StringBuilder sb = new StringBuilder(magazine);
+
+        for (char c : ransomNote.toCharArray()) {
+            int idx = sb.indexOf(String.valueOf(c));
+            if (idx == -1) {
+                return false;
+            }
+            sb.deleteCharAt(idx);
+        }
+        return true;
+    }
+}
