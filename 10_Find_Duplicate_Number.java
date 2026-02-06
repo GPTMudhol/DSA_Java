c988bfe3ca87ae17b8e313c3b536e7ff19451a96
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-06 08:49:24 +0530

    Add method to find duplicate in array
    Implement brute force approach to find duplicate number.

--- 10_Find_Duplicate_Number.java
+++ 10_Find_Duplicate_Number.java
@@ -1,1 +1,18 @@
+// Brute Force Approach 
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                return num;
+            }
+            map.put(num, 1);
+        }
+        return -1;
+    }
+}
