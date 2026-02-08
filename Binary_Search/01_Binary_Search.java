54ffa064c63b6999fd1775d3746a632fb1bdcdb1
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-08 21:30:25 +0530

    Implement binary search in 01_Binary_Search.java
    Added binary search implementation alongside linear search.

--- Binary_Search/01_Binary_Search.java
+++ Binary_Search/01_Binary_Search.java
@@ -13,3 +13,24 @@ class Solution {
     }
 }
 
+// Better (Iterative Binary Search)
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int binarySearch(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
