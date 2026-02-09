933fbf9bb595f6413cc9f032449d580627b84181
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-09 20:37:34 +0530

    Optimize searchRange for first and last positions
    Refactored searchRange method to use optimal approach for finding first and last positions of target.

--- Binary_Search/03_First_And_Last_Position.java
+++ Binary_Search/03_First_And_Last_Position.java
@@ -44,3 +44,48 @@ class Solution {
         return -1;
     }
 }
+
+// Optumal Approach 
+// Time Complexity O(log n)
+// Space Complexity O(1)
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] res = {-1, -1};
+        res[0] = firstOcc(nums, target);
+        res[1] = lastOcc(nums, target);
+        return res;
+    }
+
+    int firstOcc(int[] nums, int target) {
+        int low = 0, high = nums.length - 1, ans = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                high = mid - 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    int lastOcc(int[] nums, int target) {
+        int low = 0, high = nums.length - 1, ans = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                low = mid + 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
