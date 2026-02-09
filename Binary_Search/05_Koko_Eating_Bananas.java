99c09125cb14d8de3ab36b478d18e41536ee38c8
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-09 20:42:04 +0530

    Update 05_Koko_Eating_Bananas.java

--- Binary_Search/05_Koko_Eating_Bananas.java
+++ Binary_Search/05_Koko_Eating_Bananas.java
@@ -26,3 +26,24 @@ class Solution {
         return nums[0];
     }
 }
+
+// Optimal Approach (Binary Search)
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int findMin(int[] nums) {
+        int low = 0, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
+}
