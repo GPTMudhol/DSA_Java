388b62744c21af46005837595afcb75db9d0c50e
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-09 20:36:30 +0530

    Optimize searchRange with binary search implementation
    Refactored searchRange method to use a binary search approach for improved efficiency.

--- Binary_Search/03_First_And_Last_Position.java
+++ Binary_Search/03_First_And_Last_Position.java
@@ -15,3 +15,32 @@ class Solution {
         return new int[]{first, last};
     }
 }
+
+// Better Approach 
+// Time Complexity O(log n + k)
+// Space Complexity O(1)
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int idx = binarySearch(nums, target);
+        if (idx == -1) return new int[]{-1, -1};
+
+        int left = idx, right = idx;
+
+        while (left - 1 >= 0 && nums[left - 1] == target) left--;
+        while (right + 1 < nums.length && nums[right + 1] == target) right++;
+
+        return new int[]{left, right};
+    }
+
+    int binarySearch(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] < target) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
