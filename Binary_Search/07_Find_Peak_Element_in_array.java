18bdf8f64d6c915c42d46fcae67d6c53e4e8fb6a
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-10 18:23:11 +0530

    Add binary search method to find peak element
    Implemented an optimal binary search approach to find the peak element in an array.

--- Binary_Search/07_Find_Peak_Element_in_array.java
+++ Binary_Search/07_Find_Peak_Element_in_array.java
@@ -17,3 +17,26 @@ class Solution {
         return -1; // never reached
     }
 }
+
+
+// Optimal Approach (Binary Search)
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low < high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
+    }
+}
