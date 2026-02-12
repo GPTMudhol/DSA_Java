971aaa895c42e855c0aaf9dd66399c8ef62efcb8
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-12 23:37:24 +0530

    Update 09_Split_Array_largest.java

--- Binary_Search/09_Split_Array_largest.java
+++ Binary_Search/09_Split_Array_largest.java
@@ -66,4 +66,46 @@ class Solution {
     }
 }
 
+// Optimal Approach (Binary Search + Greedy)
+// Time Complexity  O(n * log(sum))
+// Space Complexity  O(1)
+
+class Solution {
+    public int splitArray(int[] nums, int k) {
+        int low = 0, high = 0;
+
+        for (int num : nums) {
+            low = Math.max(low, num);
+            high += num;
+        }
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (canSplit(nums, k, mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+
+    private boolean canSplit(int[] nums, int k, int maxSum) {
+        int count = 1;
+        int sum = 0;
+
+        for (int num : nums) {
+            if (sum + num > maxSum) {
+                count++;
+                sum = num;
+            } else {
+                sum += num;
+            }
+        }
+
+        return count <= k;
+    }
+}
 
