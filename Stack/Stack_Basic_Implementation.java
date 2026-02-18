576f8b2ac058889a76e7f0281566960f765a9f7a
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-18 21:39:34 +0530

    Update Stack_Basic_Implementation.java

--- Stack/Stack_Basic_Implementation.java
+++ Stack/Stack_Basic_Implementation.java
@@ -61,3 +61,21 @@ public class Main {
     }
 }
 
+
+// Stack Using Deque
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+    public static void main(String[] args) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+    }
+}
