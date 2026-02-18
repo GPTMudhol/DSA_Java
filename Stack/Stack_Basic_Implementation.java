946a957acbe0c672f75b9bde51d51176b1e7039a
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-18 21:27:51 +0530

    Update Stack_Basic_Implementation.java

--- Stack/Stack_Basic_Implementation.java
+++ Stack/Stack_Basic_Implementation.java
@@ -1,1 +1,43 @@
+// Stack Implementation Using Array
 
+class Stack {
+    int[] arr;
+    int top;
+    int capacity;
+
+    Stack(int size) {
+        capacity = size;
+        arr = new int[size];
+        top = -1;
+    }
+
+    // Push Operation
+    void push(int x) {
+        if (top == capacity - 1) {
+            System.out.println("Stack Overflow");
+            return;
+        }
+        arr[++top] = x;
+    }
+
+    // Pop Operation
+    int pop() {
+        if (top == -1) {
+            System.out.println("Stack Underflow");
+            return -1;
+        }
+        return arr[top--];
+    }
+
+    // Peek Operation
+    int peek() {
+        if (top == -1) {
+            return -1;
+        }
+        return arr[top];
+    }
+
+    boolean isEmpty() {
+        return top == -1;
+    }
+}
