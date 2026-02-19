c42e2eb38ac344424eddd14b6f56a1683f00741a
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-19 21:14:20 +0530

    Update 03_Min_Stack.java

--- Stack/03_Min_Stack.java
+++ Stack/03_Min_Stack.java
@@ -18,3 +18,44 @@ class Solution {
     }
 }
 
+
+// Better Approach
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (char ch : s.toCharArray()) {
+            
+            switch (ch) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(ch);
+                    break;
+                    
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(')
+                        return false;
+                    break;
+                    
+                case '}':
+                    if (stack.isEmpty() || stack.pop() != '{')
+                        return false;
+                    break;
+                    
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[')
+                        return false;
+                    break;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
