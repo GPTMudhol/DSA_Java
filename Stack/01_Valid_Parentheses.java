80dfe826d30713f201ab70d5a8771f9717f5bce1
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-19 21:08:51 +0530

    Update 01_Valid_Parentheses.java

--- Stack/01_Valid_Parentheses.java
+++ Stack/01_Valid_Parentheses.java
@@ -1,5 +1,5 @@
 // Brute Force Approach 
-// Time Complexity for(push,pop,top) = O(1)
+// Time Complexity for(push,pop,top) = O(1),getmin() O(n)
 // Space Complexity = O(n)
 
 import java.util.*;
@@ -30,5 +30,46 @@ class MinStack {
             min = Math.min(min, num);
         }
         return min;
+    }
+}
+
+
+// Optimal Approach 
+// Time Complexity O(1)
+// getmin O(1)
+// Space Complexity O(n)
+
+import java.util.*;
+
+class MinStack {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.peek().equals(minStack.peek())) {
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
     }
 }
