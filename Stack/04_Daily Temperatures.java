38247234aa9db49100c1b25dfc501b763ad84b82
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-20 19:25:53 +0530

    Update 04_Daily Temperatures.java

--- Stack/04_Daily Temperatures.java
+++ Stack/04_Daily Temperatures.java
@@ -1,1 +1,21 @@
+// Brute Force Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
 
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (temperatures[j] > temperatures[i]) {
+                    result[i] = j - i;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
