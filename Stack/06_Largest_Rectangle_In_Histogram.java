d78cf1e43b6f9ab5a59727d5ef8c55b144a5f9eb
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-21 22:11:19 +0530

    Update 06_Largest_Rectangle_In_Histogram.java

--- Stack/06_Largest_Rectangle_In_Histogram.java
+++ Stack/06_Largest_Rectangle_In_Histogram.java
@@ -1,1 +1,31 @@
+// Brute Force Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(1)
 
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int maxArea = 0;
+
+        for (int i = 0; i < n; i++) {
+            int height = heights[i];
+            int left = i;
+            int right = i;
+
+            // expand left
+            while (left >= 0 && heights[left] >= height) {
+                left--;
+            }
+
+            // expand right
+            while (right < n && heights[right] >= height) {
+                right++;
+            }
+
+            int width = right - left - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+
+        return maxArea;
+    }
+}
