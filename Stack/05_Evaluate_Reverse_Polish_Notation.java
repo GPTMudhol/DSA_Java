c6800d10da0686941f11bd47d746d3a6b04495c9
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-21 08:08:44 +0530

    Update 05_Evaluate_Reverse_Polish_Notation.java

--- Stack/05_Evaluate_Reverse_Polish_Notation.java
+++ Stack/05_Evaluate_Reverse_Polish_Notation.java
@@ -1,1 +1,46 @@
+// Brute Force Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
 
+import java.util.*;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+
+        List<String> list = new ArrayList<>(Arrays.asList(tokens));
+
+        while (list.size() > 1) {
+
+            for (int i = 0; i < list.size(); i++) {
+
+                String token = list.get(i);
+
+                if (isOperator(token)) {
+
+                    int a = Integer.parseInt(list.get(i - 2));
+                    int b = Integer.parseInt(list.get(i - 1));
+
+                    int result = 0;
+
+                    if (token.equals("+")) result = a + b;
+                    else if (token.equals("-")) result = a - b;
+                    else if (token.equals("*")) result = a * b;
+                    else result = a / b;
+
+                    list.remove(i);
+                    list.remove(i - 1);
+                    list.remove(i - 2);
+
+                    list.add(i - 2, String.valueOf(result));
+                    break;
+                }
+            }
+        }
+        return Integer.parseInt(list.get(0));
+    }
+
+    private boolean isOperator(String s) {
+        return s.equals("+") || s.equals("-") ||
+               s.equals("*") || s.equals("/");
+    }
+}
