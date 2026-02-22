031383f63799c6b6840f77c5f1c6dcf03f476554
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-22 20:59:03 +0530

    Update O7_asteroid collision_.java

--- Stack/O7_asteroid collision_.java
+++ Stack/O7_asteroid collision_.java
@@ -36,3 +36,42 @@ public int[] asteroidCollision(int[] asteroids) {
 
     return list.stream().mapToInt(i -> i).toArray();
 }
+
+
+// Better Approach (Stack)
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+public int[] asteroidCollision(int[] asteroids) {
+
+    Stack<Integer> stack = new Stack<>();
+
+    for (int asteroid : asteroids) {
+
+        boolean destroyed = false;
+
+        while (!stack.isEmpty() && asteroid < 0 && stack.peek() > 0) {
+
+            if (stack.peek() < -asteroid) {
+                stack.pop();
+                continue;
+            } else if (stack.peek() == -asteroid) {
+                stack.pop();
+            }
+
+            destroyed = true;
+            break;
+        }
+
+        if (!destroyed) {
+            stack.push(asteroid);
+        }
+    }
+
+    int[] result = new int[stack.size()];
+    for (int i = stack.size() - 1; i >= 0; i--) {
+        result[i] = stack.pop();
+    }
+
+    return result;
+}
