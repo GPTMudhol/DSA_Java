5b483b5ce5e8107dbfec57711b5566a78fd4dc16
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-23 22:20:39 +0530

    Update 08_Simplify_Path_Problem.java

--- Stack/08_Simplify_Path_Problem.java
+++ Stack/08_Simplify_Path_Problem.java
@@ -35,3 +35,42 @@ public class SimplifyPathBrute {
         return result.length() == 0 ? "/" : result;
     }
 }
+
+
+// Better Approach
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+import java.util.*;
+
+public class SimplifyPathBetter {
+
+    public static String simplifyPath(String path) {
+
+        String[] parts = path.split("/");
+        List<String> list = new ArrayList<>();
+
+        for (String part : parts) {
+
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            }
+
+            if (part.equals("..")) {
+                if (!list.isEmpty()) {
+                    list.remove(list.size() - 1);
+                }
+            } else {
+                list.add(part);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for (String dir : list) {
+            result.append("/").append(dir);
+        }
+
+        return result.length() == 0 ? "/" : result.toString();
+    }
+}
