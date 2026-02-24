63c45c17b58743b0a2bd46b257b699a886461256
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-24 08:08:30 +0530

    Update 09_Online_Stock_Span.java

--- Stack/09_Online_Stock_Span.java
+++ Stack/09_Online_Stock_Span.java
@@ -1,1 +1,27 @@
+// Brute Force Approach
+// Time Complexity: O(n^2)
+// Space Complexity: O(n)
 
+import java.util.*;
+
+class StockSpanner {
+
+    private List<Integer> prices;
+
+    public StockSpanner() {
+        prices = new ArrayList<>();
+    }
+
+    public int next(int price) {
+        prices.add(price);
+        int span = 1;
+        int i = prices.size() - 2;
+
+        while (i >= 0 && prices.get(i) <= price) {
+            span++;
+            i--;
+        }
+
+        return span;
+    }
+}
