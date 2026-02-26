a422469d37deeaf391ffa5d114e02f523e193d48
Nagaraj Kamatagi <143863270+GPTMudhol@users.noreply.github.com>
2026-02-26 20:51:21 +0530

    Update 01_Linked_List_Cycle.java

--- LinkedList/01_Linked_List_Cycle.java
+++ LinkedList/01_Linked_List_Cycle.java
@@ -1,1 +1,30 @@
+// Brute Force Approach
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 
+import java.util.HashSet;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+public class LinkedListCycleBrute {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> set = new HashSet<>();
+        
+        while (head != null) {
+            if (set.contains(head)) {
+                return true; // Cycle detected
+            }
+            set.add(head);
+            head = head.next;
+        }
+        
+        return false; // No cycle
+    }
+}
